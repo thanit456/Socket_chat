a9e5eb181af108552b876464507e35f43fef9cdd
thanit456 <too_w_o@hotmail.com>
2020-02-25 17:14:31 +0700

    can access multiple users

--- client_member.py
+++ client_member.py
@@ -7,9 +7,10 @@ import sys
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
-server_address = ('localhost', 10000)
-print('connecting to {} port {}'.format(*server_address))
-sock.connect(server_address)
+# server_address = ('localhost', 10000)
+# print('connecting to {} port {}'.format(*server_address))
+# sock.connect(server_address)
+sock.connect(('localhost', 50007))
 try:
     username = input("Please input username : ").strip()
     register_message = b'REG' + username.encode('utf-8')
--- server.py
+++ server.py
@@ -1,48 +1,54 @@
 import socket
+from _thread import * 
+import threading
+
+def connection_func(con, addr):
+    print('Connection : ', con)
+    
+    while True:
+        data = con.recv(1024)
+        print('received {!r}'.format(data))
+        if data:
+            print('sending message back to the client')
+            if data[:3] == b'REG':
+                username = data[3:].decode()
+                username_dc[addr] = username
+                print('GOT USERNAME : ', username)
+                print(username_dc)
+                message = data[3:] + b'is connected'
+            else: 
+                message = 'server got data'    
+            con.sendall(data)
+        else:
+            print('no data from', addr)
+            break
+    con.close()
 
 # global variables 
-username_list = dict()
-
+username_dc = dict()
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind the socket to the port
-server_address = ('localhost', 10000)
-print('Starting up on {} port {}'.format(*server_address))
-sock.bind(server_address)
+# server_address = ('localhost', 10000)
+# print('Starting up on {} port {}'.format(*server_address))
+sock.bind(('', 50007))
 
 # Listen for incoming connections
-sock.listen(1)
+sock.listen(10)
+
+# Blocking
+sock.setblocking(1)
 
 while True:
     # Wait for a connection
     print('waiting for a connection')
     connection, client_address = sock.accept()
-    try:
-        print('connection from', client_address)
-
-        # Receive the data in small chunks and retransmit it
-        while True:
-            data = connection.recv(16)
-            print('received {!r}'.format(data))
-
-            if data:
-                print('sending message back to the client')
-                if data[:3] == b'REG':
-                    username = data[3:].decode()
-                    username_list.append(username)
-                    print('GOT USERNAME : ', username)
-                    print(username_list)
-                    message = data[3:] + b'is connected'
-                else: 
-                    message = 'server got data'    
-                connection.sendall(data)
-            else:
-                print('no data from', client_address)
-                break
-
-    finally:
-        # Clean up the connection
-        print("Closing current connection")
-        connection.close()
+  
+    print('connection from', client_address)
+    start_new_thread(connection_func, (connection, client_address, ))
+    # connection_func(connection, client_address)
+    # Clean up the connection
+print("Closing socket")
+sock.close()
